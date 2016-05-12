4cfdba94b1c04bf8d9e1f7adbc043e8682922522
theelous3 <theegrandmaster@gmail.com>
2016-05-12 05:57:06 +0100

    Add files via upload

--- reddit_api_fondler.py
+++ reddit_api_fondler.py
@@ -12,14 +12,19 @@ class RedditScraper:
         self.last_id = ''
 
     def get_new_subs(self):
+        raylway = 1
         for subm in self.raw_obj.get_subreddit(c_.bot_config.subreddit).get_new():
             if subm.id == self.last_id:
                 break
             time_delta = time.time() - subm.created_utc
             if time_delta >= 300 and time_delta < 6000:
                 if subm.score >= 1:
-                    self.last_id = subm.id
-                    yield subm.url
+                    if raylway > 0:
+                        self.last_id = subm.id
+                        raylway -= 1
+                        yield subm.url
+                    else:
+                        yield subm.url
         
             
 
